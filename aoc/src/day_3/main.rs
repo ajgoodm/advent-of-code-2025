7c6baad29ce31c03caa378eeb98932a967e1405b
Aaron Aster <aarongoodm@gmail.com>
2025-12-03 10:02:25 -0500

    part 2

--- aoc/src/day_3/main.rs
+++ aoc/src/day_3/main.rs
@@ -7,23 +7,32 @@ fn main() {
             "aoc/src/day_3/data/part_1.txt"
         )))
     );
-    println!("part 2: {}", part_2());
+    println!(
+        "part 2: {}",
+        part_2(parse_banks(AocBufReader::from_string(
+            "aoc/src/day_3/data/part_1.txt"
+        )))
+    );
 }
 
-fn _part_1_joltage(bank: Vec<usize>) -> usize {
-    let len = bank.len();
-    let (idx, first_digt) = index_of_max(&bank[..(len - 1)]);
-    let (_, second_digit) = index_of_max(&bank[(idx + 1)..]);
+fn _joltage(bank: &[usize], n_digits: usize) -> usize {
+    if n_digits == 1 {
+        let (_, max) = index_of_max(bank);
+        return *max;
+    }
 
-    10 * *first_digt + *second_digit
+    let len = bank.len();
+    let (idx, digit) = index_of_max(&bank[..(len - (n_digits - 1))]);
+    10usize.pow((n_digits - 1).try_into().unwrap()) * digit
+        + _joltage(&bank[(idx + 1)..], n_digits - 1)
 }
 
 fn part_1(banks: Vec<Vec<usize>>) -> usize {
-    banks.into_iter().map(_part_1_joltage).sum()
+    banks.into_iter().map(|bank| _joltage(&bank[..], 2)).sum()
 }
 
-fn part_2() -> usize {
-    0
+fn part_2(banks: Vec<Vec<usize>>) -> usize {
+    banks.into_iter().map(|bank| _joltage(&bank[..], 12)).sum()
 }
 
 fn parse_banks(iter: impl Iterator<Item = String>) -> Vec<Vec<usize>> {
@@ -50,5 +59,18 @@ mod tests {
         .into_iter()
         .map(|x| x.to_string());
         assert_eq!(part_1(parse_banks(input)), 357);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = [
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
+        ]
+        .into_iter()
+        .map(|x| x.to_string());
+        assert_eq!(part_2(parse_banks(input)), 3121910778619);
     }
 }
