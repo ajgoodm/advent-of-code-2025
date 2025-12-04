61c6eedbefd26adec244f4ee3c4510b6d17e701e
Aaron Aster <aarongoodm@gmail.com>
2025-12-04 06:19:59 -0500

    part 2

--- aoc/src/day_4/main.rs
+++ aoc/src/day_4/main.rs
@@ -9,7 +9,10 @@ fn main() {
         "part 1: {}",
         part_1(AocBufReader::from_string("aoc/src/day_4/data/part_1.txt"))
     );
-    println!("part 2: {}", part_2());
+    println!(
+        "part 2: {}",
+        part_2(AocBufReader::from_string("aoc/src/day_4/data/part_1.txt"))
+    );
 }
 
 fn count_neighbors(x: &Coord2D<usize>, all: &HashSet<Coord2D<usize>>) -> usize {
@@ -26,13 +29,28 @@ fn part_1(reader: impl Iterator<Item = String>) -> usize {
     let paper_rolls = grid.find('@');
     paper_rolls
         .iter()
-        .map(|x| count_neighbors(x, &paper_rolls))
-        .filter(|x| *x < 4)
+        .filter(|x| count_neighbors(x, &paper_rolls) < 4)
         .count()
 }
 
-fn part_2() -> usize {
-    0
+fn part_2(reader: impl Iterator<Item = String>) -> usize {
+    let grid: Grid<char> = Grid::from_line_iter(reader);
+    let mut paper_rolls = grid.find('@');
+    let mut result = 0;
+
+    loop {
+        let to_remove = paper_rolls
+            .iter()
+            .filter(|x| count_neighbors(x, &paper_rolls) < 4)
+            .cloned()
+            .collect::<HashSet<_>>();
+        if to_remove.is_empty() {
+            break;
+        }
+        result += to_remove.len();
+        paper_rolls.retain(|x| !to_remove.contains(x));
+    }
+    result
 }
 
 #[cfg(test)]
@@ -40,7 +58,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_() {
+    fn test_part_1() {
         assert_eq!(
             part_1(
                 [
@@ -59,6 +77,29 @@ mod tests {
                 .map(|x| x.to_string())
             ),
             13
+        )
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part_2(
+                [
+                    "..@@.@@@@.",
+                    "@@@.@.@.@@",
+                    "@@@@@.@.@@",
+                    "@.@@@@..@.",
+                    "@@.@@@@.@@",
+                    ".@@@@@@@.@",
+                    ".@.@.@.@@@",
+                    "@.@@@.@@@@",
+                    ".@@@@@@@@.",
+                    "@.@.@@@.@.",
+                ]
+                .into_iter()
+                .map(|x| x.to_string())
+            ),
+            43
         )
     }
 }
